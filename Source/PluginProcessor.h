b9bd8f4c848183e5f306996a377d6846b2f16c25
TomTHN <schmidtom@gmx.net>
2023-11-17 18:55:05 +0100

    Setting up DSP Order FIFO

--- Source/PluginProcessor.h
+++ Source/PluginProcessor.h
@@ -9,6 +9,7 @@
 #pragma once
 
 #include <JuceHeader.h>
+#include <Fifo.h>
 
 //==============================================================================
 /**
@@ -65,7 +66,7 @@ public:
         END_OF_LIST
     };
     
-    using DSP_Order = std::array<DSP_Option, static_cast<size_t>(DSP_Option::END_OF_LIST)>;
+    using DSP_Order = std::array<DSP_Option, static_cast<size_t>(DSP_Option::END_OF_LIST)>; SimpleMBComp::Fifo<DSP_Order> dspOrderFifo;
     
     using DSP_Pointers = std::array<juce::dsp::ProcessorBase*, static_cast<size_t>(DSP_Option::END_OF_LIST)>;
 
